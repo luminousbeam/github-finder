8e5775c9cad96b98dee78b2756095436aff636e7
luminousbeam <jocelyn.jeriah@gmail.com>
2020-04-26 19:03:00 -0400

    refactor User to functional component, add proptypes, render spinner if loading

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,8 +1,17 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-export const Spinner = () => {
+
+const Spinner = () => {
+  return (
     <Fragment>
-        <img src={spinner} alt="Loading..." style={{width="200px", margin="auto", display:"block"}}/>
+      <img
+        src={spinner}
+        alt='Loading...'
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
     </Fragment>
+  );
 };
+
+export default Spinner;
