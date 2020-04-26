f6bd57ea29fdf804393c3186eb5adcc4184e46c1
luminousbeam <jocelyn.jeriah@gmail.com>
2020-04-25 21:52:05 -0400

    set title and icon default props and PropTypes as strings

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Navbar extends Component {
+    static defaultProps = {
+        title: "Github Finder",
+        icon: "fab fa-github"
+    }
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    }
+
     render() {
         return (
             <nav className="navbar bg-primary">
                 <h1>
-                    <i className="fab fa-github"></i> Navbar
+                    <i className={this.props.icon}></i> {this.props.title}
                 </h1>
             </nav>
         )
