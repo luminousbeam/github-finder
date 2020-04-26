37ba3c2182856743572d71528dab36d202e5d12d
luminousbeam <jocelyn.jeriah@gmail.com>
2020-04-25 22:40:56 -0400

    implement Prettier formatting to files, add dummy state data for UserItem card

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
-    static defaultProps = {
-        title: "Github Finder",
-        icon: "fab fa-github"
-    }
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github',
+  };
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
 
-    render() {
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    <i className={this.props.icon}></i> {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon}></i> {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
