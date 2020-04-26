d2a557169094f60ae6f3f1aa7099b70a83b202ba
luminousbeam <jocelyn.jeriah@gmail.com>
2020-04-26 19:20:05 -0400

    create .env.local file to hold Github API client_id and client_secret, implement these variables to github url upon mount

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ users: res.data, loading: false });
   }
 
@@ -21,7 +23,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
