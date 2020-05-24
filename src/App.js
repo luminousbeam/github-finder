6b99f712c4fe869c66473817ac73e7ee99dd8616
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-24 17:29:59 -0400

    pass Search props up to Apps.js, query Github API for users, search functionality available now

--- src/App.js
+++ src/App.js
@@ -19,12 +19,20 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  //search Github users
+  searchUsers = async (text) => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
