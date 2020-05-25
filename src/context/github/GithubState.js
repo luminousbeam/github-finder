7f1e14f4e4c4fc265f714a66ce5055a17be470bc
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-25 16:34:59 -0400

    started to implement context, added types

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,46 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './GithubContext';
+import GithubReducer from './GithubReducer';
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+} from '../types';
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  //Search users
+
+  //get user
+
+  //get repos
+
+  //clear users
+
+  //set loading
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        loading: state.loading,
+        repos: state.repos,
+      }}>
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubState;
