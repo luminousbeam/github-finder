2e6944ae4f25fa680c47b40518943755cd3f29a1
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-25 18:30:46 -0400

    created Home functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import About from './components/pages/About';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -20,16 +19,7 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
               </Switch>
