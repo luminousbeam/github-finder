c4481d6b50151951c0a31c8061e7f4eef7e090ce
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-25 18:21:49 -0400

    moved Alert state to context workflow

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,9 +1,11 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/alert/AlertContext';
 
-const Search = ({ setAlert }) => {
+const Search = () => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
   const [text, setText] = useState('');
 
   const onChange = (e) => setText(e.target.value);
@@ -11,7 +13,7 @@ const Search = ({ setAlert }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      setAlert('Please enter text', 'light');
+      alertContext.setAlert('Please enter text', 'light');
     } else {
       githubContext.searchUsers(text);
       setText('');
@@ -43,10 +45,6 @@ const Search = ({ setAlert }) => {
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  setAlert: PropTypes.func.isRequired,
 };
 
 export default Search;
